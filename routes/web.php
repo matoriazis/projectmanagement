32575c781eb396326b7fe02a8109c7f6a6a9fce8
ilyasa666 <ilyas.paradima@gmail.com>
2022-01-20 11:11:53 +0700

    iya

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\DeveloperController;
+use App\Http\Controllers\ClientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,8 +26,16 @@ Route::get('/', function () {
 Route::middleware(['auth'])->group(function () {
     Route::middleware(['access.admin'])->prefix('admin')->group(function() {
         Route::get('/dashboard', [DashboardController::class, 'admin'])->name('admin.dashboard');
+        Route::get('/tickets', [TicketController::class, 'index'])->name('admin.ticket.index');
+        Route::get('/tickets/create', [TicketController::class, 'create'])->name('admin.ticket.editor');
+        Route::get('/project', [ProjectController::class, 'index'])->name('admin.project.index');
+        Route::get('/project/create', [ProjectController::class, 'create'])->name('admin.project.editor');
+        Route::get('/developer', [DeveloperController::class, 'index'])->name('admin.developer.index');
+        Route::get('/developer/create', [DeveloperController::class, 'create'])->name('admin.developer.editor');
+        Route::get('/client', [ClientController::class, 'index'])->name('admin.client.index');
+        Route::get('/client/create', [ClientController::class, 'create'])->name('admin.client.editor');
     });
-    Route::middleware(['access.client'])->prefix('chief')->group(function() {
+    Route::middleware(['access.client'])->prefix('client')->group(function() {
         Route::get('/dashboard', [DashboardController::class, 'client'])->name('client.dashboard');
     });
     Route::middleware(['access.developer'])->group(function() {
